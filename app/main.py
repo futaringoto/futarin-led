d98edfb9f7c630896b7d5418add25e74ae6ca769
Merucu <cykps.ps@gmail.com>
2024-10-02 01:22:56 +0900

    feature: Set HTTP server port number

--- app/main.py
+++ app/main.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
             return jsonify({"status": "response fail"}), 202
 
         if __name__ == "__main__":
-            app.run(debug=True)
+            app.run(debug=True, port=8080)
 
     except KeyboardInterrupt:
         pass
