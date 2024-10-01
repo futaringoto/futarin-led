9217294c55ed66a9643199bc56880fafe816655b
cykps <cykps.ps@gmail.com>
2024-10-02 01:00:44 +0900

    fix: add indent

--- app/futarin_led.py
+++ app/futarin_led.py
@@ -57,20 +57,19 @@ class Flash(threading.Thread):
 
     def run(self):
         self.strip.setPixelColor(12, 0)
-
-    while not self.should_stop:
-        for q in range(12):
-            for i in range(0, self.strip.numPixels(), 12):
-                self.strip.setPixelColor(i + q, self.color)
-            self.strip.show()
-            time.sleep(self.wait_ms / 1000.0)
-        time.sleep(1)
-        for q in range(12):
-            for i in range(0, self.strip.numPixels(), 12):
-                self.strip.setPixelColor(i + q, 0)
-            self.strip.show()
-            time.sleep(self.wait_ms / 1000.0)
-        time.sleep(1)
+        while not self.should_stop:
+            for q in range(12):
+                for i in range(0, self.strip.numPixels(), 12):
+                    self.strip.setPixelColor(i + q, self.color)
+                self.strip.show()
+                time.sleep(self.wait_ms / 1000.0)
+            time.sleep(1)
+            for q in range(12):
+                for i in range(0, self.strip.numPixels(), 12):
+                    self.strip.setPixelColor(i + q, 0)
+                self.strip.show()
+                time.sleep(self.wait_ms / 1000.0)
+            time.sleep(1)
 
     def stop(self):
         self.should_stop = True
